4c58b7fd1969a05c348b983d0f88e1815633492a
unenglishable <unenglishable@gmail.com>
2017-03-23 18:10:59 -1000

    stub boards test
    this is the first example of a test; it is not correct at the moment, as
    it does not set up its own data in a `before` clause

---
+++ test/boards.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var config = require(path.join(__dirname, 'config'));
+var chakram = require('chakram'), expect = chakram.expect;
+
+var routes = {
+  allCategories: {
+    method: 'GET',
+    path: config.host + 'api/boards'
+  }
+};
+
+describe("[public] Boards", function() {
+  it("should return all boards", function () {
+    return chakram.get(routes.allCategories.path)
+    .then(function(response) {
+      var body = response.body;
+      // check the boards
+      expect(response).to.have.status(200);
+      expect(body).to.have.property('boards');
+      expect(body.boards).to.be.an.array;
+      expect(body.boards).to.have.length(4);
+      // check the threads
+      expect(body).to.have.property('threads');
+      expect(body.threads).to.be.an.array;
+      expect(body.threads).to.have.length(0);
+    });
+  });
+});
