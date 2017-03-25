16cd88b39c82c20237cc8400a3b70f0932e24db2
unenglishable <unenglishable@gmail.com>
2017-03-24 19:37:27 -1000

    add a delete user test

--- test/auth.js
+++ test/auth.js
@@ -21,6 +21,17 @@ var routes = {
   }
 };
 
+var utilityRoutes = {
+  deleteUser: {
+    method: 'DELETE',
+    path: config.host + 'api/users'
+  },
+  findTestUser: {
+    method: 'GET',
+    path: `${config.host}api/users/user`
+  }
+};
+
 describe("Auth", function() {
   it("logs in a user", function() {
     return chakram.post(routes.login.path, utils.admin.credentials)
@@ -64,6 +75,46 @@ describe("Auth", function() {
 
       var roles = body.roles;
       expect(avatar).to.be.an.array;
+    });
+  });
+  it("deletes a user", function () {
+    // log in with admin account and get created user id
+    var todo = [
+      chakram.post(routes.login.path, utils.admin.credentials),
+      chakram.get(utilityRoutes.findTestUser.path)
+    ];
+    return chakram.all(todo)
+    .spread(function(adminResponse, userResponse) {
+      expect(adminResponse).to.have.status(200);
+      expect(adminResponse).to.have.property('body');
+      expect(adminResponse.body).to.have.property('token');
+      var adminToken = adminResponse.body.token;
+
+      expect(userResponse).to.have.status(200);
+      expect(userResponse).to.have.property('body');
+      expect(userResponse.body).to.have.property('id');
+      var userId = userResponse.body.id;
+
+      var params = {
+        headers: {
+          'Authorization': `BEARER ${adminToken}`
+        }
+      };
+      return chakram.delete(`${utilityRoutes.deleteUser.path}/${userId}`, {}, params);
+    })
+    .then(function(response) {
+      expect(response).to.have.status(200);
+      expect(response).to.have.property('body');
+
+      var body = response.body;
+      expect(response.body).to.have.property('username');
+      expect(response.body).to.have.property('email');
+
+      var username = body.username;
+      expect(username).to.equal(routes.register.data.username);
+
+      var email = body.email;
+      expect(email).to.equal(routes.register.data.email);
     });
   });
 });
