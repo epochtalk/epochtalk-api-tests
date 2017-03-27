ffb0079d0b013f9061ce47f733c82efaefbeb8eb
unenglishable <unenglishable@gmail.com>
2017-03-27 12:40:15 -1000

    add routes file
    describes routes to be used in tests

---
+++ routes.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var config = require(path.join(__dirname, 'config'));
+
+var routes = module.exports = {
+  auth: {
+    register: {
+      method: 'POST',
+      path: config.host + 'api/register',
+      data: {
+        username: 'user',
+        email: 'test@epochtalk.com',
+        password: 'password',
+        confirmation: 'password'
+      }
+    },
+    login: {
+      method: 'POST',
+      path: config.host + 'api/login'
+    }
+  },
+  users: {
+    deleteUser: {
+      method: 'DELETE',
+      path: config.host + 'api/users'
+    },
+    findTestUser: {
+      method: 'GET',
+      path: `${config.host}api/users/user`
+    }
+  }
+};
