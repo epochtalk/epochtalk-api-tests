4accf73c68e8b33611d0b497f71876ea0d3e9cf1
unenglishable <unenglishable@gmail.com>
2017-03-27 12:32:05 -1000

    move delete user test to users.js

---
+++ test/users.js
@@ -0,0 +1,104 @@
+var path = require('path');
+var utils = require(path.join(__dirname, 'utils'));
+var config = require(path.join(__dirname, '..', 'config'));
+var chakram = require('chakram'), expect = chakram.expect;
+
+var routes = {
+  allCategories: {
+    method: 'GET',
+    path: config.host + 'api/boards'
+  },
+  login: {
+    method: 'POST',
+    path: config.host + 'api/login'
+  },
+  register: {
+    method: 'POST',
+    path: config.host + 'api/register',
+    data: {
+      username: 'user',
+      email: 'test@epochtalk.com',
+      password: 'password',
+      confirmation: 'password'
+    }
+  }
+};
+
+var utilityRoutes = {
+  deleteUser: {
+    method: 'DELETE',
+    path: config.host + 'api/users'
+  },
+  findTestUser: {
+    method: 'GET',
+    path: `${config.host}api/users/user`
+  }
+};
+
+describe("User Delete", function() {
+  // create the user to delete
+  before(function() {
+    return chakram.post(routes.register.path, routes.register.data)
+    .then(function(response) {
+      expect(response).to.have.status(200);
+
+      var body = response.body;
+      expect(body).to.have.property('token');
+      expect(body).to.have.property('id');
+      expect(body).to.have.property('avatar');
+      expect(body).to.have.property('roles');
+
+      var token = body.token;
+      expect(token).to.be.a.string;
+
+      var id = body.id;
+      expect(id).to.be.a.string;
+
+      var avatar = body.avatar;
+      expect(avatar).to.be.a.string;
+
+      var roles = body.roles;
+      expect(avatar).to.be.an.array;
+    });
+  });
+  it("deletes a user", function () {
+    // log in with admin account and get created user id
+    var todo = [
+      chakram.post(routes.login.path, utils.admin.credentials),
+      chakram.get(utilityRoutes.findTestUser.path)
+    ];
+    return chakram.all(todo)
+    .spread(function(adminResponse, userResponse) {
+      expect(adminResponse).to.have.status(200);
+      expect(adminResponse).to.have.property('body');
+      expect(adminResponse.body).to.have.property('token');
+      var adminToken = adminResponse.body.token;
+
+      expect(userResponse).to.have.status(200);
+      expect(userResponse).to.have.property('body');
+      expect(userResponse.body).to.have.property('id');
+      var userId = userResponse.body.id;
+
+      var params = {
+        headers: {
+          'Authorization': `BEARER ${adminToken}`
+        }
+      };
+      return chakram.delete(`${utilityRoutes.deleteUser.path}/${userId}`, {}, params);
+    })
+    .then(function(response) {
+      expect(response).to.have.status(200);
+      expect(response).to.have.property('body');
+
+      var body = response.body;
+      expect(response.body).to.have.property('username');
+      expect(response.body).to.have.property('email');
+
+      var username = body.username;
+      expect(username).to.equal(routes.register.data.username);
+
+      var email = body.email;
+      expect(email).to.equal(routes.register.data.email);
+    });
+  });
+});
