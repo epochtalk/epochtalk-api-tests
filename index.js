077cabc3103117be01e5fca51ff3361e12442b65
unenglishable <unenglishable@gmail.com>
2017-03-27 12:33:10 -1000

    run users test

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var path = require('path');
 var test = require(path.join(__dirname, 'test'));
 
 require(test.auth);
+require(test.users);
 require(test.boards);
--- test/index.js
+++ test/index.js
@@ -2,5 +2,6 @@ var path = require('path');
 
 module.exports = {
   auth: path.join(__dirname, 'auth'),
+  users: path.join(__dirname, 'users'),
   boards: path.join(__dirname, 'boards')
 };
