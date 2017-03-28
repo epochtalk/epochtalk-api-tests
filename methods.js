1834f9192964127e5672f5591361bee03f078cd9
unenglishable <unenglishable@gmail.com>
2017-03-27 14:45:34 -1000

    add auth/register to methods

--- methods.js
+++ methods.js
@@ -4,6 +4,9 @@ var routes = require(path.join(__dirname, 'routes'));
 
 module.exports = {
   auth: {
+    register: function(username, email, password, confirmation) {
+      return chakram.post(`${routes.auth.register.path}`, { username, email, password, confirmation });
+    },
     login: function(username, password) {
       return chakram.post(`${routes.auth.login.path}`, { username, password });
     }
