9f7fd5f48d6601d2cab4cf270ca5c634243cfc7d
unenglishable <unenglishable@gmail.com>
2017-04-18 15:58:53 -1000

    test api/authenticate

--- test/auth.js
+++ test/auth.js
@@ -121,3 +121,62 @@ describe("Auth Username Availability", function() {
     return utils.deleteUser(userInfo.username);
   });
 });
+
+describe("Auth Authenticate", function() {
+  var userInfo = {
+    username: 'user',
+    email: 'test@epochtalk.com',
+    password: 'password',
+    confirmation: 'password'
+  };
+
+  before("register a user", function() {
+    return auth.register(userInfo.username, userInfo.email, userInfo.password, userInfo.confirmation)
+    .then(function() {
+      return auth.login(userInfo.username, userInfo.password);
+    })
+    .then(function(response) {
+      userInfo.token = response.body.token;
+    });
+  });
+  it("checks a user's authentication", function() {
+    return auth.authenticate(userInfo.token)
+    .then(function(response) {
+      expect(response).to.have.status(200);
+
+      var body = response.body;
+      expect(body).to.have.all.keys([
+        'id',
+        'username',
+        'avatar',
+        'roles',
+        'moderating',
+        'permissions',
+        'token'
+      ]);
+
+      var id = response.body.id;
+      expect(id).to.be.a.slugid;
+
+      var username = response.body.username;
+      expect(username).to.be.a('string');
+
+      var avatar = response.body.avatar;
+      expect(avatar).to.be.a('string');
+
+      var roles = response.body.roles;
+      expect(roles).to.be.an('array');
+
+      var moderating = response.body.moderating;
+      expect(moderating).to.be.an('array');
+
+      var permissions = response.body.permissions;
+
+      var token = response.body.token;
+      expect(token).to.be.a('string');
+    });
+  });
+  after("delete created user", function() {
+    return utils.deleteUser(userInfo.username);
+  });
+});
