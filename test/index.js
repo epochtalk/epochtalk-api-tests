4afe214a36a4cf3bb583f72fa59cd8caac3c5612
unenglishable <unenglishable@gmail.com>
2017-04-20 15:09:05 -1000

    add portal test to test/index

--- test/index.js
+++ test/index.js
@@ -3,5 +3,6 @@ var path = require('path');
 module.exports = {
   auth: path.join(__dirname, 'auth'),
   users: path.join(__dirname, 'users'),
-  boards: path.join(__dirname, 'boards')
+  boards: path.join(__dirname, 'boards'),
+  portal: path.join(__dirname, 'portal')
 };
