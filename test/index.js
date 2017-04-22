87ed54b6da34da5617387bfd3d6d47e40457dce6
unenglishable <unenglishable@gmail.com>
2017-04-21 17:00:36 -1000

    export email in test/index

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var path = require('path');
 
 module.exports = {
+  email: path.join(__dirname, 'email'),
   auth: path.join(__dirname, 'auth'),
   users: path.join(__dirname, 'users'),
   boards: path.join(__dirname, 'boards'),
