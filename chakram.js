375e2ecf9c9acc8189f3ae15557b7396b549e412
unenglishable <unenglishable@gmail.com>
2017-04-25 15:55:55 -1000

    add someProperties method to custom chai extensions
    checks if an array contains an object with some properties

--- chakram.js
+++ chakram.js
@@ -1,6 +1,8 @@
+var _ = require('lodash');
 var isUuid= require('uuid-validate');
 var slugid = require('slugid');
 var chakram = module.exports = require('chakram');
+var expect = chakram.expect;
 
 chakram.addProperty('uuidv4', function(obj) {
   this.assert(isUuid(obj, 4), `expected ${obj} to be a uuidv4`, `expected ${obj} not to be a uuidv4`);
@@ -8,4 +10,8 @@ chakram.addProperty('uuidv4', function(obj) {
 
 chakram.addProperty('slugid', function(slug) {
   this.assert(isUuid(slugid.decode(slug), 4), `expected ${slug} to be a slugidv4`, `expected ${slug} not to be a slugidv4`);
+});
+
+chakram.addMethod('someProperties', function(array, properties) {
+  this.assert(_.some(array, properties), `expected ${array} to have some properties ${properties}`);
 });
