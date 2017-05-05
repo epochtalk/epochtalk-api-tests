5283fff35d14e1d2fd32709a6ab9585fda5dee39
unenglishable <unenglishable@gmail.com>
2017-05-04 19:15:22 -1000

    add test for updating tos

--- test/admin/legal.js
+++ test/admin/legal.js
@@ -52,3 +52,45 @@ describe("Legal page reset", function() {
     });
   });
 });
+
+describe("Legal page update", function() {
+  var testText = {
+    tos: 'The tos text should now contain this string.',
+    privacy: 'The privacy text should now contain this string.',
+    disclaimer: 'The disclaimer text should now contain this string.'
+  };
+  afterEach("reset legal text", function() {
+    return utils.sudo().then(function(response) {
+      var adminToken = response.body.token;
+      return legal.reset(adminToken);
+    })
+  });
+  it("should update tos for the legal page", function () {
+    var adminToken;
+    return utils.sudo().then(function(response) {
+      adminToken = response.body.token;
+      return legal.update({ tos: testText.tos }, adminToken);
+    })
+    .then(function(response) {
+      expect(response).to.have.status(200);
+      return legal.text(adminToken);
+    })
+    .then(function(response) {
+      expect(response).to.have.status(200);
+
+      var legalText = response.body;
+      expect(legalText).to.have.all.keys(['tos', 'privacy', 'disclaimer']);
+
+      var tos = legalText.tos;
+      expect(tos).to.be.a('string')
+      .and.to.equal(testText.tos);
+
+      // returns the string 'undefined' for undefined fields
+      var privacy = legalText.privacy;
+      expect(privacy).to.equal('undefined');
+
+      var disclaimer = legalText.disclaimer;
+      expect(disclaimer).to.equal('undefined');
+    });
+  });
+});
