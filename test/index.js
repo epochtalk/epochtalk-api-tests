061bd1a7f9e24313def703a6904abfce8ef84f66
unenglishable <unenglishable@gmail.com>
2017-05-04 19:26:14 -1000

    add admin tests to test/index

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var path = require('path');
 
 module.exports = {
+  admin: path.join(__dirname, 'admin'),
   email: path.join(__dirname, 'email'),
   auth: path.join(__dirname, 'auth'),
   users: path.join(__dirname, 'users'),
