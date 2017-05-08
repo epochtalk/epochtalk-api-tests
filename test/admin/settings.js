7028d877cd9a9c26cc2ea784d138ebe77e150ff3
unenglishable <unenglishable@gmail.com>
2017-05-08 13:18:32 -1000

    add test for return of admin settings

---
+++ test/admin/settings.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var chakram = require(path.join(__dirname, '..', '..', 'chakram')), expect = chakram.expect;
+
+var utils = require(path.join(__dirname, '..', '..', 'utils'));
+var methods = require(path.join(__dirname, '..', '..', 'methods'));
+var settings = methods.admin.settings;
+
+describe("Settings find", function() {
+  it("should return the settings", function () {
+    return utils.sudo().then(function(response) {
+      var adminToken = response.body.token;
+      return settings.find(adminToken);
+    })
+    .then(function(response) {
+      expect(response).to.have.status(200);
+
+      var legalText = response.body;
+      expect(legalText).to.have.all.keys([
+        'emailer',
+        'ga_key',
+        'images',
+        'invite_only',
+        'log_enabled',
+        'login_required',
+        'portal',
+        'rate_limiting',
+        'saas_mode',
+        'verify_registration',
+        'website'
+      ]);
+    });
+  });
+});
